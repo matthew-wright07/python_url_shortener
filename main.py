3dbbfcd13e24da81e7289299de32901520b0594e
Matthew <matthewjwright07@gmail.com>
2025-05-06 15:20:06 -0500

    updates

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ url = input("Please enter a url to shorten.\n")
 shortener = pyshorteners.Shortener()
 short_url = shortener.tinyurl.short(url)
 
-print(short_url)
+print(f"Here is your shortened url: {short_url}")
